81aae92fdc64a415f817deb5daaafeed5a8725ec
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2023-01-15 12:43:48 -0500

    server and frontend changes

---
+++ Client/src/components/pictures/Pictures.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, FC, ReactElement } from "react";
+import { AiOutlineCamera } from "react-icons/ai";
+
+// const unsplash = new Unsplash({
+//   accessKey: process.env.KEY,
+// });
+
+const Pictures: FC = (): ReactElement => {
+  //   const [query, setQuery] = useState("");
+
+  //   const Pictures = async (e) => {
+  //     e.preventDefault();
+  //     unsplash.Search.photos(query)
+  //       .then(toJson)
+  //       .then((json) => {
+  //         console.log(json);
+  //       });
+  //   };
+  return (
+    //     <>
+    //       <form className="form">
+    //         <label className="label" htmlFor="query">
+    //           {" "}
+    //           <AiOutlineCamera />
+    //         </label>
+    //         <input
+    //           type="text"
+    //           name="query"
+    //           className="input"
+    //           placeholder={`Try "dog" or "apple"`}
+    //           value={query}
+    //           onChange={(e) => setQuery(e.target.value)}
+    //         />
+    //         <button type="submit" className="button">
+    //           Search
+    //         </button>
+    //       </form>
+    //     </>
+    <div>hello</div>
+  );
+};
+
+export default Pictures;
