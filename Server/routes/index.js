76edd0d14b88e14be8971a18c3a7f2313e0f8f1d
Brandon Extra <brandex@Brandons-MacBook-Air.local>
2023-01-15 15:06:34 -0500

    deploy to heroku

--- Server/routes/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = (app) => {
-  app.use("/login", require("./login"));
-  app.use("/signup", require("./signup"));
-};
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -3,10 +3,10 @@ const Login = require("../models/login");
 const router = express.Router();
 
 //GET SPECIFIC
-router.get("/login:id", async (req, res) => {
+router.get("/login/:id", (req, res) => {
   try {
-    let foundlogin = await Login.findById(req.params.id);
-    res.status(200).send(foundlogin);
+    let foundlogin = Login.findById(req.params.id);
+    res.status(200).send("found");
   } catch (error) {
     res.status(500).json({ message: "Error Retreiving Login" });
     console.log(error);
